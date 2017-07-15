99c138f00c9d23a4bc3ed63fe762ad061f6e8a25
Torki Alsobhi <tmr630@gmail.com>
2017-07-15 15:47:34 +0300

    Initial commit

---
+++ app/src/main/java/com/torki/andriod/broburgerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.torki.andriod.broburgerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
